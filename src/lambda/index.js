27c2edd57d1992c38046ea76d0592280c1ab9286
Ely Alamillo <ely0013@gmail.com>
2018-12-22 00:44:04 -0600

    fixed return error

--- src/lambda/index.js
+++ src/lambda/index.js
@@ -2,6 +2,6 @@ exports.handler = (event, ctx, cb) => {
   // cb(errorHandler, succesHandler)
   cb(null, {
     statusCode: 200,
-    body: 'Hello world'
+    body: 'Hello worlds'
   });
 };
--- src/lambda/users.js
+++ src/lambda/users.js
@@ -4,9 +4,9 @@ exports.handler = (event, ctx, cb) => {
   axios
     .get('https://jsonplaceholder.typicode.com/users')
     .then(x => {
-      cb(null, { statusCode: 200, body: x.data });
+      cb(null, { statusCode: 200, body: JSON.stringify({ users: x.data }) });
     })
     .catch(err => {
-      cb({ error: err });
+      cb({ error: JSON.stringify(err) });
     });
 };
